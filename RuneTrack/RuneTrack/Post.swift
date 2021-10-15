16d1aa7cd751a7b2a57264d347cab4a3e9f440b9
LunaPerry <lunaleeperry@gmail.com>
2021-10-15 07:58:00 -0700

    created post model

---
+++ RuneTrack/RuneTrack/Post.swift
@@ -0,0 +1,100 @@
+//
+//  Post.swift
+//  RuneTrack
+//
+//  Created by Luna Perry on 10/15/21.
+//
+
+import Foundation
+
+//
+
+struct Stats {
+    var Overall = ["Rank": 0,
+                   "Level": 0,
+                   "Experience": 0]
+    var Attack = ["Rank": 0,
+                  "Level": 0,
+                  "Experience": 0]
+    var Defence = ["Rank": 0,
+                   "Level": 0,
+                   "Experience": 0]
+    var Strength = ["Rank": 0,
+                    "Level": 0,
+                    "Experience": 0]
+    var Constitution = ["Rank": 0,
+                        "Level": 0,
+                        "Experience": 0]
+    var Ranged = ["Rank": 0,
+                  "Level": 0,
+                  "Experience": 0]
+    var Prayer = ["Rank": 0,
+                  "Level": 0,
+                  "Experience": 0]
+    var Magic = ["Rank": 0,
+                 "Level": 0,
+                 "Experience": 0]
+    var Cooking = ["Rank": 0,
+                   "Level": 0,
+                   "Experience": 0]
+    var Woodcutting = ["Rank": 0,
+                       "Level": 0,
+                       "Experience": 0]
+    var Fletching = ["Rank": 0,
+                     "Level": 0,
+                     "Experience": 0]
+    var Fishing = ["Rank": 0,
+                   "Level": 0,
+                   "Experience": 0]
+    var Firemaking = ["Rank": 0,
+                      "Level": 0,
+                      "Experience": 0]
+    var Crafting = ["Rank": 0,
+                    "Level": 0,
+                    "Experience": 0]
+    var Smithing = ["Rank": 0,
+                    "Level": 0,
+                    "Experience": 0]
+    var Mining = ["Rank": 0,
+                  "Level": 0,
+                  "Experience": 0]
+    var Herblore = ["Rank": 0,
+                    "Level": 0,
+                    "Experience": 0]
+    var Agility = ["Rank": 0,
+                   "Level": 0,
+                   "Experience": 0]
+    var Thieving = ["Rank": 0,
+                    "Level": 0,
+                    "Experience": 0]
+    var Slayer = ["Rank": 0,
+                  "Level": 0,
+                  "Experience": 0]
+    var Farming = ["Rank": 0,
+                   "Level": 0,
+                   "Experience": 0]
+    var Runecrafting = ["Rank": 0,
+                        "Level": 0,
+                        "Experience": 0]
+    var Hunter = ["Rank": 0,
+                  "Level": 0,
+                  "Experience": 0]
+    var Construction = ["Rank": 0,
+                        "Level": 0,
+                        "Experience": 0]
+    var Summoning = ["Rank": 0,
+                     "Level": 0,
+                     "Experience": 0]
+    var Dungeoneering = ["Rank": 0,
+                         "Level": 0,
+                         "Experience": 0]
+    var Divination = ["Rank": 0,
+                      "Level": 0,
+                      "Experience": 0]
+    var Invention = ["Rank": 0,
+                     "Level": 0,
+                     "Experience": 0]
+    var Archaeology = ["Rank": 0,
+                       "Level": 0,
+                       "Experience": 0]
+}
