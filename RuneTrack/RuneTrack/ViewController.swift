55a73108bc05ffe0e720654a01af05ca735bfcf2
LunaPerry <lunaleeperry@gmail.com>
2021-10-15 14:21:46 -0700

    Added network layer and fixed bugs

--- RuneTrack/RuneTrack/ViewController.swift
+++ RuneTrack/RuneTrack/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         updateTable()
-        print(person)
     }
     
     override func viewDidLoad() {
@@ -42,20 +41,60 @@ class ViewController: UIViewController {
     
     func fetchPerson(from userInput: String, completion: @escaping (Result<Person, Error>) -> Void) {
         // make reqeust to API
-
+        
         // send request
 
         // decode data from API
-        let jsonString = "38379,2875,1857656003 15987,99,108935961 42130,99,104332304 24058,99,102291776 22484,99,172160749 41666,99,85601060 28989,99,25588785 20763,99,131883726 41672,99,22465402 80054,99,16538676 38543,99,18714956 46296,99,33189710 26682,99,45466805 56554,99,18392625 9867,99,104915376 116171,99,16496762 18606,120,110107822 26053,99,29889808 78890,99,18223996 16219,120,133055157 61943,111,46340173 86982,99,13941861 23586,99,42227006 37919,99,20739462 13894,99,104828863 68222,113,52707807 40039,99,21636003 15364,120,200000000 54884,113,56983372 -1,-1 -1,-1 25384,4115009 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 21867,109 -1,-1 -1,-1 86092,9955 14357,94 44262,13 36998,162 130663,12 47484,13" // overall and attack from API
-        let stats = makeStats(from: jsonString)
-        let person = Person(username: userInput, stats: stats)
+        
+        // let jsonString = "38379,2875,1857656003 15987,99,108935961 42130,99,104332304 24058,99,102291776 22484,99,172160749 41666,99,85601060 28989,99,25588785 20763,99,131883726 41672,99,22465402 80054,99,16538676 38543,99,18714956 46296,99,33189710 26682,99,45466805 56554,99,18392625 9867,99,104915376 116171,99,16496762 18606,120,110107822 26053,99,29889808 78890,99,18223996 16219,120,133055157 61943,111,46340173 86982,99,13941861 23586,99,42227006 37919,99,20739462 13894,99,104828863 68222,113,52707807 40039,99,21636003 15364,120,200000000 54884,113,56983372 -1,-1 -1,-1 25384,4115009 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 -1,-1 21867,109 -1,-1 -1,-1 86092,9955 14357,94 44262,13 36998,162 130663,12 47484,13"
+        
+        let urlSession = URLSession.shared
 
-        completion(.success(person))
+        
+        
+        // func getStats(completion: @escaping (Result<[Stats], Error>) -> Void) {
+            let query = usernameTextField.text!
+            
+           // let jsonString = ""
+            var baseURL = "https://secure.runescape.com/m=hiscore/index_lite.ws?player=\(query)"
+            let fullURL = URL(string: baseURL)!
+            
+            var request = URLRequest(url: fullURL)
+            
+            request.httpMethod = "GET"
+            
+            let task = urlSession.dataTask(with: request) { data, response, error in
+                // error check/handling
+               if let error = error {
+                   print(error.localizedDescription)
+                   return
+               }
+
+               // make sure we get back data
+               guard let data = data else {
+                   return
+               }
+                
+                guard let result = String(data: data, encoding: .utf8) else {
+                       return
+                   }
+                
+                let stats = self.makeStats(from: result)
+               let person = Person(username: userInput, stats: stats)
+                DispatchQueue.main.async {
+                    completion(.success(person))
+                }
+            
+              }
+            task.resume()
+            
+       // }
+    
     }
 
     func makeStats(from string: String) -> [Stats] {
         let skillNames = ["Overall", "Attack", "Defence", "Strength", "Constitution", "Ranged", "Prayer", "Magic", "Cooking", "Woodcutting", "Fletching", "Fishing", "Firemaking", "Crafting", "Smithing", "Mining", "Herblore", "Agility", "Thieving", "Slayer", "Farming", "Runecrafting", "Hunter", "Construction", "Summoning", "Dungeoneering", "Divination", "Invention", "Archaeology"]
-        var skillStringArray = string.split(separator: " ")
+        var skillStringArray = string.split(separator: "\n")
 
         
         var results = [Stats]()
@@ -98,3 +137,4 @@ extension ViewController: UITableViewDataSource {
 extension ViewController: UITableViewDelegate {
   // Code to handle cell events goes here...
 }
+
