2db65a79d2064cf7821143d46a2c49d8160ab396
LunaPerry <lunaleeperry@gmail.com>
2021-10-15 08:06:38 -0700

    added feed view

--- RuneTrack/RuneTrack/ViewController.swift
+++ RuneTrack/RuneTrack/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UITableViewDataSource {
+   /// Determines how many cells will be shown on the table view.
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 29
+   }
+
+   /// Creates and configures each cell.
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       return UITableViewCell()
+  }
+}
+extension ViewController: UITableViewDelegate {
+  // Code to handle cell events goes here...
+}
